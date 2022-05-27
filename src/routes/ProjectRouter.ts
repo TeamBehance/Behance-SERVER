5b092cf203e3165ecec3563ff7244b2413ab2af6
dingding-21 <phj8221@naver.com>
2022-05-27 11:34:10 +0900

    [FEAT] Create project api

--- src/routes/ProjectRouter.ts
+++ src/routes/ProjectRouter.ts
@@ -1,5 +1,17 @@
 import express, { Router } from 'express';
+import { body } from 'express-validator';
+import ProjectController from '../controllers/ProjectController';
 
 const router: Router = express.Router();
+
+router.post(
+  '/ios',
+  [
+    body('title').notEmpty(),
+    body('photo').notEmpty(),
+    body('writer').notEmpty(),
+  ],
+  ProjectController.createProject
+);
 
 export default router;
--- src/services/ProjectService.ts
+++ src/services/ProjectService.ts
@@ -1,1 +1,39 @@
-export {};
+import { ProjectResponseDto } from '../interfaces/project/ProjectResponseDto';
+import { ProjectInfo } from '../interfaces/project/ProjectInfo';
+import Project from '../models/Project';
+import User from '../models/User';
+
+const createProject = async (
+  projectInfo: ProjectInfo
+): Promise<ProjectResponseDto | null> => {
+  try {
+    const project = new Project({
+      title: projectInfo.title,
+      photo: projectInfo.photo,
+      writer: projectInfo.writer,
+    });
+
+    await project.save();
+
+    const user = await User.findById(projectInfo.writer);
+
+    if (!user) {
+      return null;
+    }
+
+    const data = {
+      _id: project._id,
+      title: project.title,
+      photo: project.photo,
+      writer: {
+        name: user.name,
+        photo: user.photo,
+      },
+    };
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+export default { createProject };
