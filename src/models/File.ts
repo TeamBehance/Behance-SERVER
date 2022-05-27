b6894fdc22923557f3f2cfce7890d10bf9849218
dingding-21 <phj8221@naver.com>
2022-05-27 11:43:34 +0900

    [FEAT] Upload image file api

---
+++ src/models/File.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { FileInfo } from '../interfaces/file/FileInfo';
+
+const FileSchema = new mongoose.Schema(
+  {
+    link: {
+      type: String,
+      required: true,
+    },
+    fileName: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model<FileInfo & mongoose.Document>('File', FileSchema);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ProjectRouter from './ProjectRouter';
 import StoryRouter from './StoryRouter';
+import FileRouter from './FileRouter';
 
 const router = Router();
 
 router.use('/project', ProjectRouter);
 router.use('/story', StoryRouter);
+router.use('/file', FileRouter);
 
 export default router;
